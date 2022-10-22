6f9a26ff7e9e8c1ce2bd163b3563851ceead82da
runedelamont <runedelamont@gmail.com>
2022-10-22 14:01:56 -0700

    initial dmr table

--- app/models/dmr.py
+++ app/models/dmr.py
@@ -4,14 +4,15 @@ class DMR(db.Model):
     __tablename__ = 'direct_message_rooms'
     
     id = db.Column(db.Integer, primary_key=True)
-    message_id = db.Column(db.Integer)
+    # Todo: reformat for multiple messages
+    # message_id = db.Column(db.Integer)
     created_at = db.Column(db.DATE)
     updated_at = db.Column(db.DATE)
     
     def to_dict(self):
         return {
             'id': self.id,
-            'message_id': self.message_id
+            # 'message_id': self.message_id
             'created_at': self.created_at
             'updated_at': self.updated_at
         }
