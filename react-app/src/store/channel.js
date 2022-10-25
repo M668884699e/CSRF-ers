1bd638e950c01f559b4224c8e69ccbe26cc0863d
chawin <pathocha000@gmail.com>
2022-10-25 14:22:47 -0700

    Updated Landing Page as of 2:22 PM 10/25/22 - CP

---
+++ react-app/src/store/channel.js
@@ -0,0 +1,8 @@
+const initialState = { user: null };
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import session from './session'
+import session from './session';
+import channel from './channel';
 
 const rootReducer = combineReducers({
   session,
+  channel,
 });
 
 
