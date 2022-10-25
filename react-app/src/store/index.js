1f1c6087ff20fe450b168d366425351c9e92d92a
runedelamont <runedelamont@gmail.com>
2022-10-25 14:45:58 -0700

    First push

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session';
 import channel from './channel';
+import dmr from './dmr';
 
 const rootReducer = combineReducers({
   session,
   channel,
+  dmr
 });
 
 
