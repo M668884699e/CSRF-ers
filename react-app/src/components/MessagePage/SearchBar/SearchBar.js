401076fd40855c335152821483d7e9f87a97ee9f
irelius <kihong711@hotmail.com>
2022-10-25 21:49:16 -0700

    fixing errors - sam

---
+++ react-app/src/components/MessagePage/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import "./SearchBar.css"
+
+const SearchBar = () => {
+    return (
+        <section>
+            <p>
+                hello world
+            </p>
+        </section>
+    )
+}
+
+export default SearchBar
