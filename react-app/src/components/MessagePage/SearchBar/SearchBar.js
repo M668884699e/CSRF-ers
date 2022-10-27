210805c3a7f807a939cca3e23c3b704a5adc5438
irelius <kihong711@hotmail.com>
2022-10-26 20:55:29 -0700

    completed wireframe of search bar with some color added as well for dark mode - sam

--- react-app/src/components/MessagePage/SearchBar/SearchBar.js
+++ react-app/src/components/MessagePage/SearchBar/SearchBar.js
@@ -2,11 +2,33 @@ import "./SearchBar.css"
 
 const SearchBar = () => {
     return (
-        <section>
-            <p>
-                hello world
-            </p>
-        </section>
+        <header id="search-bar-main">
+            <section id="left-bar-section">
+                <section id="left-section">
+                    LEFT
+                </section>
+            </section>
+            <section id="center-bar-section">
+                <button id="clock-button">
+                    C
+                </button>
+                <section id="search-bar">
+                    <section id="search-text">
+                        Search App Academy
+                    </section>
+                    <section id="search-buttons">
+                        <button>P</button>
+                        <button>S</button>
+                    </section>
+                </section>
+            </section>
+            <section id="right-bar-section">
+                <section id="right-section">
+                    <button id="help-button">?</button>
+                    <button>PRF</button>
+                </section>
+            </section>
+        </header>
     )
 }
 
