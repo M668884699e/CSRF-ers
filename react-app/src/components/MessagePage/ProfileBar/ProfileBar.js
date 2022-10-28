2881ee68fd574f2e2bcc159eee2e16d8d1769010
chawin <pathocha000@gmail.com>
2022-10-28 15:07:47 -0700

    Updated Profile Bar | 3:07 PM 10.28.22 - CP

--- react-app/src/components/MessagePage/ProfileBar/ProfileBar.js
+++ react-app/src/components/MessagePage/ProfileBar/ProfileBar.js
@@ -8,38 +8,107 @@ import { useSelector } from "react-redux"
 import * as sessionActions from '../../..//store/session';
 
 const ProfileBar = () => {
-    // get current user profile image
+    // get current user information
     const profilePicture = useSelector(sessionActions.getUserProfilePicture)
     const firstName = useSelector(sessionActions.getUserFirstName);
     const lastName = useSelector(sessionActions.getUserLastName);
     const username = useSelector(sessionActions.getUserDisplayName);
+    const emailAddress = useSelector(sessionActions.getUserEmail);
 
-    console.log("profilePicture", profilePicture);
+    // get current time
+    const currentTime = () => {
+        let currDate = new Date();
+        let H = currDate.getHours();
+        let m = currDate.getMinutes();
+        let amPm = "AM";
+
+        // change to appropriate time format 
+        if(currDate.getHours() >= 12) amPm = "PM"
+        if (H < 10) H = "0" + H;
+        if (H > 12) H = H - 12;
+        if (m < 10) m = "0" + m;
+        
+        document.getElementById("profile-curr-time").textContent = `${H}:${m} ${amPm} local time`
+    }
+
+    // update current time
+    setInterval(currentTime);
+
+    
 
     return (
         <section id="profile-bar">
             <section id="profile-bar-header">
                 <h2>
                     Profile
+                    <i className="fa-solid fa-x pb-x-icon fa-xs"></i>
                 </h2>
             </section>
+            {/* Aside Containers */}
             <section id="profile-bar-aside-container">
+                {/* Aside 1 */}
                 <aside id="pbac-a1">
+                    {/* Aside 1 Container 1 */}
                     <figure id="pbac-a1-c1">
                         <img
                             src={profilePicture}
                             alt={username}
                         />
                     </figure>
+
+                    {/* Aside 1 Container 2 */}
                     <section id="pbac-a1-c2">
                         <p>{firstName + " " + lastName}</p>
+                        <p>Edit</p>
+                    </section>
+
+                    {/* Aside 1 Container 3 */}
+                    <section id="pbac-a1-c3">
+                        <p id="pbac-a1-c3-p">
+                            <i
+                                id="pbac-a1-c3-p-i"
+                                className="fa-regular fa-circle fa-2xs"
+                            />
+                            <span>Away</span>
+                        </p>
+                        <p id="pbac-a1-c3-p2">
+                            <i className="fa-regular fa-clock fa-xs" />
+                            <span id="profile-curr-time"/>
+                        </p>
                     </section>
                 </aside>
+                {/* line break */}
+                <span id="pbac-lb"/>
+
+                {/* Aside 2 */}
                 <aside id="pbac-a2">
-                    Section 3
-                </aside>
-                <aside id="pbac-a3">
-                    Section 4
+                    {/* Aside 2 Container 1 */}
+                    <section id="pbac-a2-c1">
+                        <p>Contact Information</p>
+                        <p>Edit</p>
+                    </section>
+
+                    {/* Aside 2 Container 2 */}
+                    <section id="pbac-a2-c2">
+                        <figure>
+                            <i className="fa-regular fa-envelope"/>
+                        </figure>
+                        <p id="pbac-a2-c2-p">
+                            Email Address
+                            <span>{emailAddress}</span>
+                        </p>
+                    </section>
+
+                    {/* Aside 2 Container 3 */}
+                    <section id="pbac-a2-c3">
+                        <figure>
+                            <i className="fa-solid fa-user"/>
+                        </figure>
+                        <p id="pbac-a2-c3-p">
+                            Display Name
+                            <span>{username}</span>
+                        </p>
+                    </section>
                 </aside>
             </section>
         </section>
