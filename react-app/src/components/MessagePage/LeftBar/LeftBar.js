24a6ed1c4186f2ffe25479389a80fd0debc71220
irelius <kihong711@hotmail.com>
2022-11-01 19:43:51 -0700

    finished react component for dmrs and created necessary files to allow function to work - sam

--- react-app/src/components/MessagePage/LeftBar/LeftBar.js
+++ react-app/src/components/MessagePage/LeftBar/LeftBar.js
@@ -4,6 +4,8 @@ import './LeftBar.css';
 // import context
 import { useChannel } from '../../../context/ChannelContext';
 import { useChannelsUsers } from '../../../context/ChannelsUsersContext';
+import { useDMR } from "../../../context/DMRContext"
+import { useDMRUsers } from "../../../context/DMRUsersContext"
 
 // import react
 import { useEffect } from 'react';
@@ -16,14 +18,17 @@ import { NavLink, useHistory } from 'react-router-dom';
 
 // import store
 import * as channelActions from '../../../store/channel';
-import * as dmrActions from '../../../store/channel';
-import * as sessionActions from '../../../store/session';
 import * as channelsUsersActions from '../../../store/channels-users';
+import * as dmrActions from '../../../store/dmr';
+import * as dmrUsersActions from "../../../store/dmr-users"
+import * as sessionActions from '../../../store/session';
 import * as userActions from '../../../store/users';
 
 const LeftBar = () => {
 	const { channels, setChannels } = useChannel();
 	const { channelsUsers, setChannelsUsers } = useChannelsUsers();
+	const { dmrs, setDMRs } = useDMR();
+	const { dmrUsers, setDMRUsers } = useDMRUsers();
 
 	// invoke dispatch
 	const dispatch = useDispatch();
@@ -31,24 +36,28 @@ const LeftBar = () => {
 	// invoke history
 	const history = useHistory();
 
-	const channelsUsersState = useSelector(
-		channelsUsersActions.getAllUsersChannels
-	);
 	const channelState = useSelector(channelActions.getAllChannels);
+	const channelsUsersState = useSelector(channelsUsersActions.getAllUsersChannels);
+	const dmrState = useSelector(dmrActions.getAllDMRs)
+	const dmrUsersState = useSelector(dmrUsersActions.getAllUserDMRs)
 	const currentUserId = useSelector(sessionActions.getCurrentUserId);
 
 	// load channels
 	useEffect(() => {
 		dispatch(channelActions.thunkGetChannels());
 		dispatch(channelsUsersActions.thunkGetAllChannelsUsers());
+		dispatch(dmrActions.thunkGetAllDmrs());
+		dispatch(dmrUsersActions.thunkGetAllDMRUsers());
 	}, [dispatch]);
 
 	// per channelState
 	useEffect(() => {
+
 		if (channelState) {
 			const currentChannelsUserBelongTo = Array.isArray(channelsUsers)
 				? channelsUsers.filter((cu) => currentUserId === cu.user_id)
 				: '';
+
 
 			const currentChannelDetail = [];
 
@@ -62,17 +71,47 @@ const LeftBar = () => {
 				channelState.filter((channel) =>
 					currentChannelDetail.includes(channel.id)
 				);
-
 			if (Array.isArray(channelDisplay)) {
 				setChannels(channelDisplay);
 			}
 		}
 	}, [channelState]);
 
+	// per dmrState
+	useEffect(() => {
+		if(dmrState) {
+			const currentDMRsUserBelongTo = Array.isArray(dmrUsers)
+			? dmrUsers.filter((dmru) => currentUserId === dmru.user_id)
+			: "";
+
+			const currentDMRDetail = [];
+
+			Array.isArray(dmrUsers) &&
+				currentDMRsUserBelongTo.forEach((dmru) => {
+					currentDMRDetail.push(dmru.dmr_id)
+				})
+
+			const dmrDisplay =
+				Array.isArray(dmrState) &&
+				dmrState.filter((dmr) =>
+					currentDMRDetail.includes(dmr.id)
+				)
+
+			if(Array.isArray(dmrDisplay)) {
+				setDMRs(dmrDisplay);
+			}
+		}
+	}, [dmrState]);
+
 	// per channelsUsers state
 	useEffect(() => {
 		setChannelsUsers(channelsUsersState);
 	}, [channelsUsersState]);
+
+	// per dmrUsers state
+	useEffect(() => {
+		setDMRUsers(dmrUsersState)
+	}, [dmrUsersState])
 
 	const loadAllChannels = () => {
 		return (
@@ -93,6 +132,26 @@ const LeftBar = () => {
 			})
 		);
 	};
+
+	const loadAllDMRs = () => {
+		return (
+			Array.isArray(dmrs) &&
+			dmrs.map((dmr, i) => {
+				return (
+					<section
+						onClick={(_) => history.push(`/chat/dmrs/${dmr.id}`)}
+						className="dmr-list-option"
+						key={i}
+					>
+						<aside>
+							<i className='fa-regular fa-hashtag'></i>
+						</aside>
+						<aside>{dmr.dmr_name}</aside>
+					</section>
+				)
+			})
+		)
+	}
 
 	return (
 		<aside id='left-bar-main'>
@@ -126,30 +185,11 @@ const LeftBar = () => {
 						<details>
 							<summary id='dmr-header'>Direct messages</summary>
 							<section id='dmr-list'>
-								<section className='dmr-list-option'>
-									<aside>
-										<i className='fa-regular fa-hashtag'></i>
-									</aside>
-									<aside>DMR 1</aside>
-								</section>
-								<section className='dmr-list-option'>
-									<aside>
-										<i className='fa-regular fa-hashtag'></i>
-									</aside>
-									<aside>DMR 2</aside>
-								</section>
-								<section className='dmr-list-option'>
-									<aside>
-										<i className='fa-regular fa-hashtag'></i>
-									</aside>
-									<aside>DMR 3</aside>
-								</section>
-								<section className='dmr-list-option'>
-									<aside>
-										<i className='fa-regular fa-plus'></i>
-									</aside>
-									<aside>Add teammates</aside>
-								</section>
+								{loadAllDMRs()}
+								<aside>
+									<i className='fa-regular fa-plus'></i>
+								</aside>
+								<aside>Add teammates</aside>
 							</section>
 						</details>
 					</section>
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import ChannelProvider from './context/ChannelContext';
+import DMRProvider from './context/DMRContext';
 import App from './App';
 import configureStore from './store';
 import { ModalProvider } from './context/Modal';
 import UserProvider from './context/UserContext';
 import ChannelsUsersProvider from './context/ChannelsUsersContext';
+import DMRUsersProvider from './context/DMRUsersContext';
 import ProfileProvider from './context/ProfileContext';
 
 const store = configureStore();
@@ -18,11 +20,15 @@ ReactDOM.render(
 			<ModalProvider>
 				<UserProvider>
 					<ChannelProvider>
-						<ChannelsUsersProvider>
-							<ProfileProvider>
-								<App />
-							</ProfileProvider>
-						</ChannelsUsersProvider>
+						<DMRProvider>
+							<ChannelsUsersProvider>
+								<DMRUsersProvider>
+									<ProfileProvider>
+										<App />
+									</ProfileProvider>
+								</DMRUsersProvider>
+							</ChannelsUsersProvider>
+						</DMRProvider>
 					</ChannelProvider>
 				</UserProvider>
 			</ModalProvider>
