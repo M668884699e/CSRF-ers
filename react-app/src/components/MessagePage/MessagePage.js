784dc1a2fbb0dd757df11012e3a23aa0bcc85c78
chawin <pathocha000@gmail.com>
2022-11-02 22:13:40 -0700

    Latest git update | 10:11 PM 11.2.22 - CP

--- react-app/src/components/MessagePage/MessagePage.js
+++ react-app/src/components/MessagePage/MessagePage.js
@@ -1,18 +1,10 @@
 // import components
-<<<<<<< HEAD
 import LeftBar from "./LeftBar"
 import ProfileBar from "./ProfileBar"
 import MessageSender from "./MessageSender"
 import MessageDisplay from "./MessageDisplay"
 import SearchBar from "./SearchBar"
 import { Editor } from "../Editor/Editor"
-=======
-import LeftBar from './LeftBar';
-import ProfileBar from './ProfileBar';
-import MessageSender from './MessageSender';
-import MessageDisplay from './MessageDisplay';
-import SearchBar from './SearchBar';
->>>>>>> dev
 
 // import context
 import MessageProvider from '../../context/MessageContext';
@@ -25,7 +17,6 @@ import './MessagePage.css';
 import React from 'react';
 
 const MessagePage = () => {
-<<<<<<< HEAD
     /**
      * Controlled Input
      */
@@ -68,44 +59,3 @@ const MessagePage = () => {
 
 
 export default MessagePage
-=======
-	/**
-	 * Controlled Input
-	 */
-	const { profileBarActive, setProfileBarActive } = useProfile();
-
-	return (
-		<div id='message-page-main'>
-			<section id='message-page-header'>
-				<SearchBar />
-			</section>
-			<section
-				id='message-page-body'
-				className={`message-page-body-profile-${profileBarActive}`}
-			>
-				{/* Left Bar */}
-				<aside id='left-bar'>
-					<MessageProvider>
-						<LeftBar />
-					</MessageProvider>
-				</aside>
-
-				{/* Message Display */}
-				<aside id='message-display'>
-					<MessageDisplay />
-				</aside>
-
-				{/* <MessageSender /> */}
-				<aside id='message-sender'></aside>
-
-				{/* Message Profile Bar */}
-				<aside id={`message-profile-${profileBarActive}`}>
-					<ProfileBar />
-				</aside>
-			</section>
-		</div>
-	);
-};
-
-export default MessagePage;
->>>>>>> dev
